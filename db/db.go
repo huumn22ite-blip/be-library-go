6f964d72012783aee06a63bcc0f52fc0a7999ba8
macchu25 <daylahuu@gmail.com>
2026-03-22 11:35:00 +0700

    fix deloy

--- db/db.go
+++ db/db.go
@@ -3,15 +3,15 @@ package db
 import (
 	"database/sql"
 	"log"
-	 _ "github.com/go-sql-driver/mysql"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var DB *sql.DB
 func Connect(){
 	var err error
-	DB,err = sql.Open(
-		"mysql", 	"root:123456@tcp(127.0.0.1:3306)/library",
-	)
+DB, err = sql.Open("mysql", os.Getenv("DB_DSN"))
 	if err != nil {
 		log.Fatal(err)
 	}
